f4ab72f7b1c360dafe4194bf7ce2a85fe5f6cd26
Pappu Dutta <pappudutta279@gmail.com>
2023-08-23 19:18:26 +0530

    feat: Update environment variables and database configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,9 @@ import Image from "next/image";
 import LoginPage from "./login/page";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <h1>Hello</h1>
+    </main>
+  );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useRef, useState } from "react";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-yellow-600">Sign Up</h1>
+    </div>
+  );
 }
 
 export default SignUp;
