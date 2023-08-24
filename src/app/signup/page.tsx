3f88b9353bea1329c4ee2adcec0d376a1712e597
Pappu Dutta <pappudutta279@gmail.com>
2023-08-24 10:00:21 +0530

    feat: Connect sign-up form to database
    - Implement API calls to store user sign-up data in the database

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,10 +3,10 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function SignUp() {
-  const router = useRouter();
+  // const router = useRouter;
   const [user, setUser] = useState({
     email: "",
     password: "",
@@ -14,12 +14,39 @@ function SignUp() {
   });
 
   const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const onSignup = async () => {};
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+
+      const response = await axios.post("/api/users/signup", user);
+      console.log(response.data);
+      // router.push("/login ");
+    } catch (error: any) {
+      console.log("Signup failed", error.message);
+
+      // toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(true);
+    } else {
+      setButtonDisabled(false);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-yellow-600">Sign Up</h1>
+      <h1 className="text-yellow-600">{loading ? "Loading..." : "Sign Up"}</h1>
       <hr />
       <label htmlFor="username" className="py-2">
         User Name
@@ -54,13 +81,22 @@ function SignUp() {
         placeholder="Password@123"
         className="text-sm text-black px-3 py-1.5 rounded-2xl focus:outline-none"
       />
-      <button
-        onClick={onSignup}
-        className="bg-blue-600 py-1.5 px-6 my-4 rounded-2xl text-sm hover:bg-blue-500 focus:outline-sky-300"
-      >
-        Sign Up
-      </button>
-      <div className="text-xs">
+      {buttonDisabled ? (
+        <button
+          onClick={onSignup}
+          className="bg-blue-600 py-1.5 px-6 my-4 rounded-2xl text-sm hover:bg-blue-500 focus:outline-sky-300"
+        >
+          Sign Up
+        </button>
+      ) : (
+        <button
+          disabled
+          className="bg-blue-200 py-1.5 px-6 my-4 rounded-2xl text-sm  focus:outline-sky-300"
+        >
+          Sign Up
+        </button>
+      )}
+      <div className="text-xs py-4">
         <span>Already SignUp? </span>
         <Link href="/login" className="text-blue-600 hover:text-blue-700">
           Log In
